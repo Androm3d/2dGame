f82607934fdc24aa5d6f643e51f60ccbd4553652
Androm3d <marcelalabart@gmail.com>
2026-03-16 11:10:02 +0100

    Include SOIL source files directly

---
+++ build/CMakeFiles/BugsBunnyCrazyCastle.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/link.d"
+  "BugsBunnyCrazyCastle"
+  "BugsBunnyCrazyCastle.pdb"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/image_dxt.c.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/image_dxt.c.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/image_helper.c.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/image_helper.c.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/soil.c.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/soil.c.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/stb_image_aug.c.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/external/soil/src/stb_image_aug.c.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Game.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Game.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Player.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Player.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Scene.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Scene.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Shader.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/ShaderProgram.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/ShaderProgram.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Sprite.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Sprite.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Texture.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/TileMap.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/TileMap.cpp.o.d"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/main.cpp.o"
+  "CMakeFiles/BugsBunnyCrazyCastle.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/BugsBunnyCrazyCastle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ images/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tiles" tilewidth="32" tileheight="32" tilecount="136" columns="8">
+ <image source="tiles.png" trans="ffffff" width="256" height="544"/>
+</tileset>
